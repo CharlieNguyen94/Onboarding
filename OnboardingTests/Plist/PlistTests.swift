5f074147bf6de38ea751336206647cda9b906ee3
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-03 18:27:02 +0100

    Unit test folder and file created.

---
+++ OnboardingTests/Plist/PlistTests.swift
@@ -0,0 +1,21 @@
+//
+//  PlistTests.swift
+//  PlistTests
+//
+//  Created by Charlie Nguyen on 03/09/2021.
+//
+
+import Foundation
+@testable import Onboarding
+import XCTest
+
+class PlistTests: XCTest {
+    
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+}
