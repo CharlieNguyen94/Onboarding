79ec2b4065206820c1e737e3ff0c6f4e44930919
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-03 12:32:42 +0100

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Charlie Nguyen on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
